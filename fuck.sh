0f460ac1494d26b171f377aa8b3b19b6aeeca8ad
kartikay-agarwal <73934694+kartikay-agarwal@users.noreply.github.com>
2022-11-21 13:43:24 +0530

    Create fuck.sh

---
+++ fuck.sh
@@ -0,0 +1,42 @@
+iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP
+iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP
+iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP
+iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP
+iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP
+iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP
+iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP
+iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+ 
+6# > Drop ICMPv4 protocol ping (Not v6) - (Good for ping-spam)
+iptables -t mangle -A PREROUTING -p icmp -j DROP
+iptables -A INPUT -p tcp -m connlimit --connlimit-above 80 -j REJECT --reject-with tcp-reset
+ 
+7# > Limit connections (If you're large network increase it)
+ 
+iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
+iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+ 
+8# > Limit TCP connections per/s
+iptables -t mangle -A PREROUTING -f -j DROP
+ 
+9# > Limit fragmentation UDP flood
+ 
+iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT
+iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+ 
+10# > SYNPROXY RULES:
+ 
+iptables -t raw -A PREROUTING -p tcp -m tcp --syn -j CT --notrack
+iptables -A INPUT -p tcp -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+ 
+11# > Avoid port scanning (Forwarding exploit)
+ 
+iptables -N port-scanning
+iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+iptables -A port-scanning -j DROP
+ 
+12# > Avoid Linux connections (OPTIONALL!!)
+iptables -A INPUT -p tcp -m tcp --dport 25565 --tcp-option 8 --tcp-flags FIN,SYN,RST,ACK SYN -j REJECT --reject-with icmp-port-unreachable
+ 
+##Install iptables-persistant for RedHat/CentOS based systems
